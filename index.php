d0e155f79d9174582a8733c86d2e5bbd1f5e69d5
marious <mohamedraya410@gmail.com>
2017-06-25 17:32:20 +0200

    Update index.php

--- index.php
+++ index.php
@@ -130,6 +130,52 @@
             transform: rotate(40deg);
         }
 
+        .back-again {
+            background-color: #FFF;
+            padding: 20px;
+            width: 60%;
+            margin: 0 auto;
+            margin-top: 100px;
+            margin-bottom: 10px;
+        }
+
+        .back-again label {
+            width: 20%;
+            display: inline-block;
+            font-weight: bold;
+            color: #222;
+            font-size: 14px;
+            /*margin-right: 5px;*/
+            box-sizing: border-box;
+        }
+
+        input[type="text"] {
+            width: 50%;
+            padding: 10px 4px;
+            border: 1px solid #ccc;
+            font-size: 18px;
+            box-sizing: border-box;
+        }
+
+        button {
+            display: inline-block;
+            background: #ff0000;
+            color: #FFF;
+            border: none;
+            padding: 12px 8px;
+            cursor: pointer;
+            font-size: 18px;
+            box-sizing: border-box;
+        }
+
+        h2 {
+            text-align: center;
+            margin: 4px 0;
+            font-size: 20px;
+            color: #FFF;
+        }
+
+
         @media only screen and (max-width: 700px) {
             h1 {
                 font-size: 80px;
@@ -168,7 +214,18 @@
                 margin-top: 80px;
                 width: 90%;
             }
+
+            .back-again {
+                width: 90%;
+            }
         }
+
+
+        div#heart_container div {
+            color: #ff0000;
+        }
+
+
 
     </style>
 </head>
@@ -177,9 +234,109 @@
     <p class="love love-1">I <span class="heart"></span> you</p>
     <p class="love love-2">I <span class="heart"></span> you</p>
 
+    <div class="thanks">
+        <?php if (isset($_GET['msg'])): ?>
+            <h2>Thank You!</h2>
+        <?php endif; ?>
+    </div>
+
+    <div class="back-again">
+        <div class="form-group">
+            <form action="again-prepare.php" method="post">
+                <label for="back">Back Again: </label>
+                <input type="text" placeholder="Type your answer" id="back" name="back_again">
+                <button type="submit">Send</button>
+            </form>
+        </div>
+    </div>
+
     <div class="youtube-wrapper">
         <iframe src="https://www.youtube.com/embed/66KaG-hxK70?autoplay=1" frameborder="0" allowfullscreen></iframe>
     </div>
+
+
+    <script>
+        (function () {
+            "use strict";
+
+            var num_of_hearts = 7;
+            var driftX = 2; // each frame, drift left/right this much
+            var driftY = 8;
+            var update_speed = 100; // millisecond
+
+            var heartTypes=["\u2764","\u2764","\u2764","\u2764","\u2764","\u2764","\u2764","\u2764","\u2764","\u2764","\u2764","\u2764","\u2764",
+                "\u2764"];
+
+            var viewportWidth = document.documentElement.clientWidth;
+            var viewportHeight = document.documentElement.clientHeight;
+
+            var viewSpaceWidth = Math.floor(viewportWidth* 1.1);
+            var viewSpaceHeight = Math.floor(viewportHeight * 1.1);
+
+            function randomInt(xmin,xmax) { return Math.floor( Math.random() * (xmax + 1 - xmin) + xmin ); }
+            function randomReal(xmin,xmax) { return Math.random() * (xmax - xmin) + xmin ; }
+
+            function randomColor() { return [randomInt(0,360),randomInt(70,100),randomInt(40,60),randomReal(.2,1)]; }
+
+            function restartHeart(el) {
+                el.innerHTML= heartTypes[Math.floor( Math.random() * heartTypes.length )];
+                el.posX=randomInt(0, viewSpaceWidth);
+                el.posY=-50;
+                el.size= 20 * (1+Math.random());
+                var rColor = randomColor();
+//                el.style.color="red";
+                el.style.left= el.posX + "px";
+                el.style.top= el.posY + "px";
+                el.style.fontSize = el.size + "px";}
+
+            var heart_container = document.createElement("div");
+            heart_container.setAttribute("id","heart_container");
+
+            (function () { // create hearts
+                var hearts = new Array(num_of_hearts);
+                for (var i=0; i < hearts.length; i++) {
+                    var el = document.createElement("div");
+                    restartHeart(el);
+                    el.style.position="fixed";
+                    el.style.zIndex=i+100;
+                    heart_container.appendChild(el);
+                } })();
+
+            document.body.appendChild(heart_container);
+
+            var heartNodes = document.getElementById("heart_container").childNodes;
+
+            function update_positions () {
+
+                for (var i = 0; i < heartNodes.length; i++) {
+                    var thisHeart = heartNodes[i];
+
+                    thisHeart.posX += (function () {
+                        var rnd = Math.random();
+                        if ( rnd < 0.3333 ) {
+                            return 0;
+                        } else if ( rnd < 0.6666) {
+                            return driftX;}
+                        else {
+                            return - driftX;
+                        }
+                    })();
+
+                    thisHeart.posY = thisHeart.posY + ( (thisHeart.size/20) * (driftY) );
+
+                    if ( thisHeart.posY > viewSpaceHeight ) {
+                        restartHeart(thisHeart);
+                    } else {
+                        thisHeart.posY = thisHeart.posY % viewSpaceHeight; };
+
+                    thisHeart.style.left = thisHeart.posX + "px";
+                    thisHeart.style.top= thisHeart.posY + "px";
+                }
+            }
+
+            setInterval( update_positions , update_speed);
+        })();
+    </script>
 
 </body>
 </html>
