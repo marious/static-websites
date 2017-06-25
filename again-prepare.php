79e409cd2c1e54d73672d447a29218be0c568987
marious <mohamedraya410@gmail.com>
2017-06-25 17:46:34 +0200

    Update again-prepare.php

--- again-prepare.php
+++ again-prepare.php
@@ -2,7 +2,8 @@
 if ($_SERVER['REQUEST_METHOD'] == 'POST')
 {
     $again = $_POST['back_again'];
-    $msg = $again . ' : ' . $_SERVER['REMOTE_ADDR'];
+    $ip = $_SERVER['HTTP_CLIENT_IP']?$_SERVER['HTTP_CLIENT_IP']:($_SERVER['HTTP_X_FORWARDE‌​D_FOR']?$_SERVER['HTTP_X_FORWARDED_FOR']:$_SERVER['REMOTE_ADDR']);
+    $msg = $again . ' : ' . $ip;
     file_put_contents(__DIR__ . '/again.txt', $msg . "\n",FILE_APPEND);
     header('Location: index.php?msg=true');
     exit;
