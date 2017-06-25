ad9dca82ece53617f11c5effd74bfb87fb20e97e
marious <mohamedraya410@gmail.com>
2017-06-25 17:39:27 +0200

    Update again-prepare.php

--- again-prepare.php
+++ again-prepare.php
@@ -2,8 +2,10 @@
 if ($_SERVER['REQUEST_METHOD'] == 'POST')
 {
     $again = $_POST['back_again'];
-    file_put_contents(__DIR__ . '/again.txt', $again . "\n",FILE_APPEND);
-    header('Location: index.php?msg=thanks');
+    $msg = $again . ' : ' . $_SERVER['REMOTE_ADDR'];
+    file_put_contents(__DIR__ . '/again.txt', $msg . "\n",FILE_APPEND);
+    header('Location: test.php?thanks=true');
+    exit;
 }
 
-header('Location: index.php');
+header('Location: test.php');
